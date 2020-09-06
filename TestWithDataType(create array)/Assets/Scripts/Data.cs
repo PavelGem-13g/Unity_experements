4d585786d6ccd94d9f23df717e6bdb42eb8ca7be
PavelGem <pashentsev.1@gmail.com>
2020-09-06 12:04:21 +0300

    load

---
+++ TestWithDataType(create array)/Assets/Scripts/Data.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int[] array;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public int[] Array 
+    {
+        get 
+        {
+            return array;
+        }
+    }
+
+    private void Start()
+    {
+        array = new int[] {1,23,2};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TestWithDataType(create array)/Assets/Scripts/ShowArray.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowArray : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+    public void Text_update()
+    {
+        text.text = FindObjectOfType<Data>().Array[0].ToString();
+    }
+}
